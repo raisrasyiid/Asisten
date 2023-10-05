a91cff31f0e879efc30fe7b7c2570e595bfd839f
raisrasyiid <raisrasyiid01@gmail.com>
2023-10-05 20:49:33 +0700

    update fragment bookKid

---
+++ app/src/main/java/com/example/myapps/BookKidActivity.kt
@@ -0,0 +1,59 @@
+package com.example.myapps
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.myapps.databinding.ActivityBookKidBinding
+
+class BookKidActivity : AppCompatActivity() {
+    lateinit var binding: ActivityBookKidBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityBookKidBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonFairy.setOnClickListener {
+            replaceFragment(FairyFragment())
+        }
+        binding.buttonFable.setOnClickListener {
+            replaceFragment(FableFragment())
+        }
+        binding.buttonScience.setOnClickListener {
+            replaceFragment(ScienceFragment())
+        }
+
+
+
+
+
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_book_kid)
+//
+//        val buttonFairy = findViewById<Button>(R.id.buttonFairy)
+//        val buttonFable = findViewById<Button>(R.id.buttonFable)
+//        val buttonScience = findViewById<Button>(R.id.buttonScience)
+//
+//        buttonFairy.setOnClickListener {
+//            val fairy = Intent(this, FairyFragment::class.java)
+//            startActivity(fairy)
+//        }
+//        buttonFable.setOnClickListener {
+//            val fable = Intent(this, FableFragment::class.java)
+//            startActivity(fable)
+//        }
+//        buttonScience.setOnClickListener {
+//            val science = Intent(this, ScienceFragment::class.java)
+//            startActivity(science)
+//        }
+    }
+    private fun replaceFragment(frg:Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTrx = fragmentManager.beginTransaction()
+        fragmentTrx.replace(R.id.fragmentKidBook,frg)
+        fragmentTrx.commit()
+    }
+}
--- app/src/main/java/com/example/myapps/HomeActivity.kt
+++ app/src/main/java/com/example/myapps/HomeActivity.kt
@@ -1,11 +1,21 @@
 package com.example.myapps
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val btnBook = findViewById<Button>(R.id.btnBookKid)
+
+        btnBook.setOnClickListener {
+            val btn = Intent(this,BookKidActivity::class.java)
+            startActivity(btn)
+        }
     }
 }
