d6ffa8171d1746c1a2724f1e0054563d44c4347b
raisrasyiid <raisrasyiid01@gmail.com>
2023-10-09 20:51:17 +0700

    update fragment kopi

--- app/src/main/java/com/example/myapps/CoffeeActivity.kt
+++ app/src/main/java/com/example/myapps/CoffeeActivity.kt
@@ -2,10 +2,25 @@ package com.example.myapps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.myapps.databinding.ActivityCoffeeBinding
 
 class CoffeeActivity : AppCompatActivity() {
+    lateinit var binding: ActivityCoffeeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_coffee)
+        binding = ActivityCoffeeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imgAll.setOnClickListener {
+            replaceFragment(AllCoffeeFragment())
+        }
+    }
+    private fun replaceFragment(frg: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTrx = fragmentManager.beginTransaction()
+        fragmentTrx.replace(R.id.linearLayout2,frg)
+        fragmentTrx.commit()
     }
 }
